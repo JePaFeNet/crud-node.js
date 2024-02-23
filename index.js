d8aee809986e404eb13663e8646fe8b18f9ed824
Jean Paul Ferreira <jeanpaul@jepafe.net>
2024-02-23 11:04:21 -0300

    Layout

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const layouts = require("express-ejs-layouts");
+
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "src/views"));
+
+app.use(layouts);
+app.set("layout", "layouts/layout");
 
 const mainRouter = require("./src/routes/main.router");
 app.use(mainRouter);
