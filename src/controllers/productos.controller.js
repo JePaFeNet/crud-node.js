0bad30cd1a73643f710dccdff8e3af45030312d0
Jean Paul Ferreira <jepafe@gmail.com>
2025-01-21 16:18:24 -0300

    CRUD MySQL

--- src/controllers/productos.controller.js
+++ src/controllers/productos.controller.js
@@ -1,20 +1,98 @@
 const querystring = require("querystring");
+const model = require("../models/Product");
 
-const index = (req, res) => {
-  const query = querystring.stringify(req.query);
-
-  fetch("https://fakestoreapi.com/products?" + query)
-    .then((res) => res.json())
-    .then((productos) => res.render("productos", { productos }));
+const create = (req, res) => {
+  res.render("productos/create");
 };
 
-const show = (req, res) => {
-  fetch("https://fakestoreapi.com/products/" + req.params.id)
-    .then((res) => res.json())
-    .then((producto) => res.json(producto));
+const store = async (req, res) => {
+  const { name } = req.body;
+
+  try {
+    const result = await model.store(name);
+    console.log(result);
+    res.redirect("/productos");
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+const index = async (req, res) => {
+  try {
+    const productos = await model.findAll();
+    res.render("productos/index", { productos });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+const show = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const producto = await model.findById(id);
+    console.log(producto);
+    if (!producto) {
+      return res.status(404).send("Producto no encontrado");
+    }
+    res.render("productos/show", { producto });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+const edit = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const producto = await model.findById(id);
+    console.log(producto);
+    if (!producto) {
+      return res.status(404).send("Producto no encontrado");
+    }
+    res.render("productos/edit", { producto });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+const update = async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  try {
+    const result = await model.update(id, name);
+    console.log(result);
+    res.redirect("/productos");
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+const destroy = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await model.destroy(id);
+    console.log(result);
+    res.redirect("/productos");
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal Server Error");
+  }
 };
 
 module.exports = {
+  create,
+  store,
   index,
   show,
+  edit,
+  update,
+  destroy,
 };
